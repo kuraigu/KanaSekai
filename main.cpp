1f10769d90c572c4555e25939d865edc44ffb53c
kuraigu <southsoftstudio@gmail.com>
2018-02-22 14:06:01 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////
+//
+// KanaSekai is a cross-platform, open-source game-like
+// learning tool for Hiragana and Katakana
+//
+// Licensed under GNU General Public License v3.0
+// (Read LICENSE for more information)
+//
+//////////////////////////////////////////////////////////////
+/// @file   	KanaSekai
+/// @author     Innoh Reloza
+/// @date       22 February 2018
+/// @version    App 0.1 Development
+///
+///////////////////////////////////////////////////////////////
+
+#include "global.h"
+
+int main()
+{
+	return 0;
+}
